3feb27771679949b0722763b9cb1cf4bc203e288
Steffen Smolka <smolka@cs.cornell.edu>
2023-12-02 18:29:16 -0800

    day 2: parsing

--- bin/day2.ml
+++ bin/day2.ml
@@ -36,7 +36,66 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green|}
 
+type color = Red | Green | Blue [@@deriving sexp, compare, equal, hash]
+type hand = color -> int
+
+let sexp_of_hand (h : hand) : Sexp.t =
+  [ (Red, h Red); (Green, h Green); (Blue, h Blue) ]
+  |> List.filter ~f:(fun (_, n) -> n <> 0)
+  |> [%sexp_of: (color * int) list]
+
+type game = Game of { id : int; hands : hand list } [@@deriving sexp_of]
+
+let empty_hand : hand = fun _ -> 0
+
+let parse_color_exn = function
+  | "red" -> Red
+  | "green" -> Green
+  | "blue" -> Blue
+  | s -> failwith (Printf.sprintf "invalid color: \"%s\"" s)
+
+let parse_game_header_exn (header : string) : int =
+  header |> String.strip
+  |> String.chop_prefix_exn ~prefix:"Game "
+  |> String.strip |> Int.of_string
+
+let merge_disjoint_hands_exn (h1 : hand) (h2 : hand) =
+  let merge_disjoint n m =
+    match (n, m) with x, 0 | 0, x -> x | _ -> failwith "not disjoint"
+  in
+  let red = merge_disjoint (h1 Red) (h2 Red) in
+  let green = merge_disjoint (h1 Green) (h2 Green) in
+  let blue = merge_disjoint (h1 Blue) (h2 Blue) in
+  function Red -> red | Green -> green | Blue -> blue
+
+let parse_hand_subset_exn (subset : string) : hand =
+  subset |> String.strip |> String.split ~on:' ' |> function
+  | [ num; color ] ->
+      let num = num |> String.strip |> Int.of_string in
+      let color = color |> String.strip |> parse_color_exn in
+      fun c -> if [%equal: color] c color then num else 0
+  | _ -> failwith "invalid"
+
+let parse_hand_exn (hand : string) : hand =
+  hand |> String.strip |> String.split ~on:','
+  |> List.map ~f:parse_hand_subset_exn
+  |> List.fold ~init:empty_hand ~f:merge_disjoint_hands_exn
+
+let parse_game_exn (game_record : string) : game =
+  match String.split game_record ~on:':' with
+  | [ header; hands ] ->
+      let id = parse_game_header_exn header in
+      let hands = String.split hands ~on:';' |> List.map ~f:parse_hand_exn in
+      Game { id; hands }
+  | _ -> Printf.sprintf "invalid game_record: \"%s\"" game_record |> failwith
+
+let parse_games_record (games_record : string) : game list =
+  games_record |> String.split_lines |> List.map ~f:parse_game_exn
+
+let test_parse_games_record () =
+  example_record |> parse_games_record |> [%sexp_of: game list]
+  |> Sexp.to_string_hum |> printf "%s\n"
+
 let () =
-  printf "Hello, %s!\n" "Steffen";
-  List.iter [ 1; 2; 3 ] ~f:(fun i -> printf "Hello, number %d" i);
-  ()
+  printf "\n------------------------------------------\n%s\n" example_record;
+  test_parse_games_record ()
