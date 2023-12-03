517ed79ff4f3f47bb7903700be66b2ee3ab99494
Steffen Smolka <smolka@cs.cornell.edu>
2023-12-02 17:09:08 -0800

    Set up ocamlformat.

--- bin/day2.ml
+++ bin/day2.ml
@@ -1,4 +1,7 @@
 open! Base
 open! Stdio
 
-let () = printf "Hello, %s!\n" "Steffen"
+let () =
+  printf "Hello, %s!\n" "Steffen";
+  List.iter [ 1; 2; 3 ] ~f:(fun i -> printf "Hello, number %d" i);
+  ()
